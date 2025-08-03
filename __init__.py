8a628bbf3ea49dfaea785b965055a4bf22d50e0b
alawryaguila <ana.aguila.18@ucl.ac.uk>
2025-08-03 10:58:51 -0400

    update and tidy example scripts

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .aekl_dataloader import *
-from .conddiff.datasets.data_utils import *
-from .conddiff.datasets.constants import *
-from .conddiff_utils import *
---
+++ scripts/__init__.py
@@ -0,0 +1,4 @@
+from .aekl_dataloader import *
+from ..conddiff.datasets.data_utils import *
+from ..conddiff.datasets.constants import *
+from ..conddiff.conddiff_utils import *
